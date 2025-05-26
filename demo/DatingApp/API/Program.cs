b59409ea2a24d05676419dd720578b4a639010ac
Jose Maria Iriarte <josemariairiarte@gmail.com>
2025-05-26 15:25:12 -0300

    Added User Controller, Data, Entities/Models, and dating.db - Modified Program.cs and config files

--- demo/DatingApp/API/Program.cs
+++ demo/DatingApp/API/Program.cs
@@ -1,22 +1,21 @@
+using System.Formats.Tar;
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
+// ADD SERVICES
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddDbContext<DataContext>(opt =>
+ {
+     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+ });
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
+// MIDDLEWARE
 
 app.MapControllers();
 

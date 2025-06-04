e5e8a9429365447a9f10d4446d4ebae160863fbc
Jose Maria Iriarte <josemariairiarte@gmail.com>
2025-06-04 13:24:12 -0300

    Creating Member Edit Component, Member Edit Template Form, Adding Update Functionality, Can Deactivate Route Guard, Persisting Changes in Member Edit to API

--- demo/DatingApp/API/Controllers/UsersController.cs
+++ demo/DatingApp/API/Controllers/UsersController.cs
@@ -1,3 +1,4 @@
+using System.Security.Claims;
 using API.Data;
 using API.DTOs;
 using API.Entities;
@@ -27,6 +28,24 @@ public class UsersController(IUserRepository userRepository, IMapper mapper) : B
         if (user == null) return NotFound();
 
         return user;
+    }
+
+    [HttpPut]
+    public async Task<ActionResult> UpdateUser(MemberUpdateDTO memberUpdateDTO)
+    {
+        var username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+        if (username == null) return BadRequest("No username found in token");
+
+        var user = await userRepository.GetUserByUsernameAsync(username);
+
+        if (user == null) return BadRequest("Could not find user");
+
+        mapper.Map(memberUpdateDTO, user);
+
+        if (await userRepository.SaveAllAsync()) return NoContent();
+
+        return BadRequest("Failed to update the user");       
     }
 
 }

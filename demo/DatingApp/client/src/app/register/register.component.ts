53787ec859a86810c13fcccbd943c4af627003f7
Jose Maria Iriarte <josemariairiarte@gmail.com>
2025-06-09 16:52:43 -0300

    Reactive Sign Up Form, Validators, Custom Validator

--- demo/DatingApp/client/src/app/register/register.component.ts
+++ demo/DatingApp/client/src/app/register/register.component.ts
@@ -1,16 +1,18 @@
-import { Component, inject, input, output } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component, inject, input, OnInit, output } from '@angular/core';
+import { AbstractControl, FormControl, FormGroup, FormsModule, ReactiveFormsModule, ValidatorFn, Validators } from '@angular/forms';
 import { AccountService } from '../_services/account.service';
 import { ToastrService } from 'ngx-toastr';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [FormsModule],
+  imports: [ReactiveFormsModule, JsonPipe],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
+  
   private accountService = inject(AccountService);
   usersFromHomeComponent = input.required<any>() // parent to child communication - input in the child component (RegisterComponent)
   // to receive the input in the html template from the parent component (Home Component)
@@ -20,17 +22,40 @@ export class RegisterComponent {
   // 3. register the output value in the child component tag in the parent component
   // 4. create the function referenced in 3 in the parent (home component)
   private toastr = inject(ToastrService);
-
   model: any = {}
+  registerForm: FormGroup = new FormGroup({})
+
+  ngOnInit(): void {
+    this.initializeForm();
+  }
+  
+  initializeForm() {
+    this.registerForm = new FormGroup({
+      username: new FormControl('Hello', Validators.required),
+      password: new FormControl('', [Validators.required, Validators.minLength(4),
+        Validators.maxLength(8)]),
+      confirmPassword: new FormControl('', [Validators.required, this.matchValues('password')])
+    });
+    this.registerForm.controls['password'].valueChanges.subscribe({
+      next: () => this.registerForm.controls['confirmPassword'].updateValueAndValidity()
+    })
+  }
+
+  matchValues(matchTo: string): ValidatorFn {
+    return (control: AbstractControl) => {
+      return control.value === control.parent?.get(matchTo)?.value ? null : {isMatching: true}
+    }
+  }
 
   register() {
-    this.accountService.register(this.model).subscribe({
-      next: response => {
-        console.log(response);
-        this.cancel();
-      },
-      error: error => this.toastr.error(error.error)
-    });
+    console.log(this.registerForm.value);
+    // this.accountService.register(this.model).subscribe({
+    //   next: response => {
+    //     console.log(response);
+    //     this.cancel();
+    //   },
+    //   error: error => this.toastr.error(error.error)
+    // });
   }
 
   cancel() {

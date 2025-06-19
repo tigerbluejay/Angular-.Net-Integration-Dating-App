45e79c1a7e4bb91b13c3722828e686fa4049c356
Jose Maria Iriarte <josemariairiarte@gmail.com>
2025-06-19 14:18:04 -0300

    Paginating the Likes on the Client

--- demo/DatingApp/client/src/app/lists/lists.component.ts
+++ demo/DatingApp/client/src/app/lists/lists.component.ts
@@ -1,26 +1,33 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
 import { Member } from '../_models/member';
 import { LikesService } from '../_services/likes.service';
 import { ButtonsModule } from 'ngx-bootstrap/buttons';
 import { FormsModule } from '@angular/forms';
 import { MemberCardComponent } from "../members/member-card/member-card.component";
+import { PaginationModule } from 'ngx-bootstrap/pagination';
 
 @Component({
   selector: 'app-lists',
   standalone: true,
-  imports: [ButtonsModule, FormsModule, MemberCardComponent],
+  imports: [ButtonsModule, FormsModule, MemberCardComponent, PaginationModule],
   templateUrl: './lists.component.html',
   styleUrl: './lists.component.css'
 })
-export class ListsComponent implements OnInit {
-  private likeService = inject(LikesService);
-  members: Member[] = [];
+export class ListsComponent implements OnInit, OnDestroy {
+
+  likesService = inject(LikesService);
   predicate = 'liked';
+  pageNumber = 1;
+  pageSize = 5;
 
   ngOnInit(): void {
     this.loadLikes();
   }
 
+  ngOnDestroy(): void {
+    this.likesService.paginatedResult.set(null);
+  }
+  
   getTitle() {
     switch (this.predicate) {
       case 'liked': return 'Members you like';
@@ -30,9 +37,14 @@ export class ListsComponent implements OnInit {
   }
 
   loadLikes() {
-    this.likeService.getLikes(this.predicate).subscribe({
-      next: members => this.members = members
-    })
+    this.likesService.getLikes(this.predicate, this.pageNumber, this.pageSize);
+  }
+
+  pageChanged(event: any) {
+    if (this.pageNumber !== event.page) {
+      this.pageNumber = event.page;
+      this.loadLikes();
+    }
   }
 
 }

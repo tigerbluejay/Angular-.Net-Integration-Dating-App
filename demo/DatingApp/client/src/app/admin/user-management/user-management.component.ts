df280254b73d4cad8f4c9ac28b967ea408247da7
Jose Maria Iriarte <josemariairiarte@gmail.com>
2025-06-27 13:11:52 -0300

    Setting up Modals, Editing Roles on the Client

--- demo/DatingApp/client/src/app/admin/user-management/user-management.component.ts
+++ demo/DatingApp/client/src/app/admin/user-management/user-management.component.ts
@@ -1,6 +1,8 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { AdminService } from '../../_services/admin.service';
 import { User } from '../../_models/user';
+import { RolesModalComponent } from '../../modals/roles-modal/roles-modal.component';
+import { BsModalRef, BsModalService, ModalOptions } from 'ngx-bootstrap/modal';
 
 @Component({
   selector: 'app-user-management',
@@ -12,10 +14,38 @@ import { User } from '../../_models/user';
 export class UserManagementComponent implements OnInit {
   
   private adminService = inject(AdminService);
+  private modalService = inject(BsModalService);
   users: User[] = [];
+  bsModalRef: BsModalRef<RolesModalComponent> = new BsModalRef<RolesModalComponent>();
 
   ngOnInit(): void {
     this.getUsersWithRoles();
+  }
+
+  openRolesModal(user: User) {
+    const initialState: ModalOptions = {
+      class: 'modal-lg',
+      initialState: {
+        title: 'User roles',
+        username: user.username,
+        selectedRoles: [...user.roles],
+        availableRoles: ['Admin', 'Moderator', 'Member'],
+        users: this.users,
+        rolesUpdated: false
+      }
+    }
+
+    this.bsModalRef = this.modalService.show(RolesModalComponent, initialState);
+    this.bsModalRef.onHide?.subscribe({
+      next: () => {
+        if (this.bsModalRef.content && this.bsModalRef.content.rolesUpdated) {
+          const selectedRoles = this.bsModalRef.content.selectedRoles;
+          this.adminService.updateUserRoles(user.username, selectedRoles).subscribe({
+            next: roles => user.roles = roles
+          })
+        }
+      }
+    })
   }
 
   getUsersWithRoles() {
